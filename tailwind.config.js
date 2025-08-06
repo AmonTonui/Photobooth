f587ca047429831e1669fa7383b9dfa23ef276c8
Tonui11 <230035780@aston.ac.uk>
2025-08-06 13:32:01 +0100

    fix: tailwind v3 directives + rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,11 @@ import typography from '@tailwindcss/typography';
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/livewire/livewire/**/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
     ],
 
     theme: {
