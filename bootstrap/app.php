1b44d3b2fbabd9d133b924d873f4ed16783a166a
Tonui11 <amontoe04@gmail.com>
2026-01-07 11:25:45 -0600

    Fix Vite assets on Railway and update TrustProxies config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,38 +1,27 @@
 <?php
 
+use App\Http\Middleware\AdminOnly;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Symfony\Component\HttpFoundation\Request as ProxyRequest; 
-use App\Http\Middleware\AdminOnly;
-
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        web: __DIR__ . "/../routes/web.php",
+        api: __DIR__ . "/../routes/api.php",
+        commands: __DIR__ . "/../routes/console.php",
+        health: "/up",
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
-
-
-        $middleware->trustProxies(
-            at: '*',
-            // headers: 
-            // ProxyRequest::HEADER_X_FORWARDED_FOR
-            // | ProxyRequest::HEADER_X_FORWARDED_HOST
-            // | ProxyRequest::HEADER_X_FORWARDED_PORT
-            // | ProxyRequest::HEADER_X_FORWARDED_PROTO
-        );
+        // Trust all proxies (Railway load balancer)
+        $middleware->trustProxies(at: "*");
 
         $middleware->alias([
-            'admin' => AdminOnly::class,
+            "admin" => AdminOnly::class,
         ]);
     })
-    ->withExceptions(fn () => null) 
+    ->withExceptions(function (Exceptions $exceptions) {
         //
+    })
     ->create();
-
-    
